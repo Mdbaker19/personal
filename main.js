b6f6a323981d0cd3fce4386ff85e792b3fd38c7b
Matthew <matthew.d.baker20@gmail.com>
2021-05-27 02:00:25 -0500

    trying to fix import error

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css'
 
-import * as THREE from 'three';
+import * as THREE from '../vite-project/node_modules/three';
 
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {OrbitControls} from '../vite-project/node_modules/three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene();
 
